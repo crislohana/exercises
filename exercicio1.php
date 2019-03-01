af132a48dfc6a810e60333d583476b83e4c9e14d
crislohana <cristiane_santos@msn.com>
2019-02-28 22:48:06 -0300

    Exer28-02-2019

---
+++ exercicio1.php
@@ -0,0 +1,19 @@
+<?php
+
+$aluno = 1;
+$somaAluno = 0;
+$notaMax = 100;
+$total = 0;
+
+echo "Digite a nota de 10 alunos.\n";
+
+while ($aluno <= 10)
+{
+    echo "Digite a nota do aluno " . $aluno . ": ";
+    $NotaAtual = rtrim(fgets(STDIN));
+    $total = $NotaAtual + $total;
+    $aluno = $aluno + 1;
+ }
+ $media = $total / 10;
+
+echo "A média é:" . $media . "\n";
